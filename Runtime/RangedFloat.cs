6a950a9081af63b721c545aa31ef4e5af0c21a57
F. Gabriele Pratticò <filippogabriele.prattico@polito.it>
2022-07-11 12:10:18 +0200

    base refactoring

---
+++ Runtime/RangedFloat.cs
@@ -0,0 +1,20 @@
+﻿
+/*
+ * Custom template by Gabriele P.
+ */
+using System;
+
+namespace PrattiToolkit
+{
+    [Serializable]
+    public struct RangedFloat
+    {
+        public float minValue;
+        public float maxValue;
+
+        public bool InRange(float value)
+        {
+            return value <= maxValue && value >= minValue;
+        }
+    }
+}
